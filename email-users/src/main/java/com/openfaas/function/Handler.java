4464da9df57b9f635d9917e52f2ef5c9b14d8a84
Joseph Harros <sc20jh@leeds.ac.uk>
2024-03-19 22:04:39 +0000

    asdfghjk

--- email-users/src/main/java/com/openfaas/function/Handler.java
+++ email-users/src/main/java/com/openfaas/function/Handler.java
@@ -95,20 +95,20 @@ public class Handler extends com.openfaas.model.AbstractHandler
         properties.put("mail.smtp.socketFactory.port", "465");
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); 
         Authenticator authenticator = null;
-         try{authenticator = new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication("jharros73@gmail.com", "vbey oobk yvro ymza");
-            }
-            };
-        }
-        catch(Exception e)
-        {
-            combine = res.getBody() + "\n" + e.getMessage();
-            res.setBody(combine);
-            return res;
-        }
+        //  try{authenticator = new Authenticator() {
+        //     protected PasswordAuthentication getPasswordAuthentication() {
+        //     return new PasswordAuthentication("jharros73@gmail.com", "vbey oobk yvro ymza");
+        //     }
+        //     };
+        // }
+        // catch(Exception e)
+        // {
+        //     combine = res.getBody() + "\n" + e.getMessage();
+        //     res.setBody(combine);
+        //     return res;
+        // }
 
-             Session session = Session.getInstance(properties, authenticator);
+        //      Session session = Session.getInstance(properties, authenticator);
 
       
         List<Story> stories = new ArrayList<Story>();
